6713fdd1983772141076f6a53e6f93cb349754ca
itslitulinchpin2 <itslitulinchpin@gmail.com>
2024-03-23 02:39:35 +0100

    add ts config directory

---
+++ config/build/main.js
@@ -0,0 +1,12 @@
+'use strict';
+class Car {
+    constructor() {
+        this.engine = 0;
+    }
+    move() {
+        const engine = this.engine + 1;
+        console.log(engine);
+    }
+}
+const car = new Car();
+car.move();
